306edd5b4c104b2f801f991505f5eee9495a8e91
Dan Zhang <zdbrenda@gmail.com>
2017-04-21 12:16:21 -0700

    Finish user edit, update, index, and destroy actions(with one failed controller test)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-  def new
-  end
+
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  	before_action :correct_user,   only: [:edit, :update]
+  	before_action :admin_user,     only: :destroy
+  
 
   def show
   	@user=User.find(params[:id])
@@ -10,7 +13,6 @@ class UsersController < ApplicationController
   def new
   	@user= User.new
   end
-
 
   def create
   	@user=User.new(user_params)
@@ -24,9 +26,45 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update_attributes(user_params)
+  		flash[:success] = "Profile updated"
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def index
+
+  	@users = User.paginate(page: params[:page])
+  end
+
+
   def destroy
-  	log_out
-  	redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  #Confirms a logged_in user.
+  def logged_in_user
+  	unless logged_in?
+  		store_location
+  		flash[:danger] = "Please log in."
+  		redirect_to login_url
+  	end
+  end
+
+  # Confirms the current user.
+  def correct_user
+  	@user = User.find(params[:id])
+  	recirect_to(root_url) unless current_user?(@user)
   end
 
   private
@@ -35,5 +73,11 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password,
   									:password_confirmation)
+  	end
+
+  	# Confirms an admin user
+
+  	def admin_user
+  		redirect_to(root_url) unless current_user.admin?
   	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Jessie",
+				email: "jessie@gmail.com",
+				password: 						"123456",
+				password_confirmation:   		"123456",
+				admin:false)
+
+User.create!(name: "Brenda Z",
+				email: "zdbrenda@gmail.com",
+				password: 						"abcdefg",
+				password_confirmation:   		"abcdefg",
+				admin:false)
+
+User.create!(name: "Example User",
+				email: "example@railsturial.org",
+				password: 						"foobar",
+				password_confirmation:   		"foobar",
+				admin:true )
+
+99.times do |n|
+	name 	= Faker::Name.name
+	email 	= "example-#{n+1}@railstutorial.org"
+	password = "password"
+	User.create!( name: 	name,
+					email:  email,
+					password: 				password,
+					password_confirmation:  password  
+					)
+end
